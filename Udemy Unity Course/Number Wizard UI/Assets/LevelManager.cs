b1ed5e81acfbb617790c2ccf4431ae91865949d9
Kevin Manning <nachyochez@gmail.com>
2016-02-10 11:37:00 -0800

    Pre class backup

---
+++ Udemy Unity Course/Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("LevelLoad() requested for " + name);
+        print("LevelLoad() requested for " + name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        print("Quit Requested");
+    }
+}
