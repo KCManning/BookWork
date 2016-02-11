1fec0d33f873913d38a3d223541c5c73c9695e93
Kevin Manning <nachyochez@gmail.com>
2016-02-11 14:41:08 -0800

    Started Menu System

---
+++ Udemy Unity Course/BrickBreaker/Assets/Scripts/Level Manager/Scene_Manager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour {
+
+    public float autoLoadNextSceneAfter;
+
+	// Use this for initialization
+	void Start () {
+        if(autoLoadNextSceneAfter != 0) { Invoke("LoadNextScene", autoLoadNextSceneAfter); }
+	
+	}
+	
+    public void LoadScene(string name)
+    {
+        Debug.Log("New Scene Load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Weapon")
+            LoadNextScene();
+    }
+
+}
