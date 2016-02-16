2436ccdb1b9ccd19631ec7ab0e4020d7f0e1b089
Kevin Manning <nachyochez@gmail.com>
2016-02-15 21:02:41 -0800

    Completed Menu System

---
+++ Udemy Unity Course/BrickBreaker/Assets/Scripts/Level Manager/ReturnHome.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReturnHome : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("Start Menu");
+	}
+}
