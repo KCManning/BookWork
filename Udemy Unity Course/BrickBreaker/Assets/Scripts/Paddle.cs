5e2632ff116daf16bc908fbe842ac8a71774521c
Kevin Manning <nachyochez@gmail.com>
2016-02-16 14:55:36 -0800

    Brick Breaker Progress

---
+++ Udemy Unity Course/BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+    int gameUnits = 16;
+    Vector3 paddlePOS;
+
+    // Use this for initialization
+    void Start () {
+        paddlePOS = new Vector3(-1f, this.transform.position.y, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePOSInBlocks = Input.mousePosition.x / Screen.width * gameUnits;
+
+        paddlePOS.x = Mathf.Clamp(mousePOSInBlocks, -7f, 7f);
+
+        print(mousePOSInBlocks + ", " + paddlePOS);
+
+
+        this.transform.position = paddlePOS;
+	}
+}
