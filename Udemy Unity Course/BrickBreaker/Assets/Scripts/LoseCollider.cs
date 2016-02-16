c5d83dec0014e3aaca078b48c35fbf2c847ffa4f
Kevin Manning <nachyochez@gmail.com>
2016-02-16 11:17:23 -0800

    Brick Breaker Menu/Sound/Collision

---
+++ Udemy Unity Course/BrickBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour
+{
+    public Scene_Manager sm;
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        Debug.Log("Collision");
+    }
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        Debug.Log("Trigger");
+        sm.LoadScene("Lose");
+    }
+}
