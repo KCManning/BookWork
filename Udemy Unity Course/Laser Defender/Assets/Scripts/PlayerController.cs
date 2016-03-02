e629e91ed56089451109b3f3167ba899ceae94b5
Kevin Manning <nachyochez@gmail.com>
2016-03-02 11:51:36 -0800

    Pre Class Backup

--- Udemy Unity Course/Laser Defender/Assets/Scripts/PlayerController.cs
+++ Udemy Unity Course/Laser Defender/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    public GameObject beamPrefab;
     public float speed = 15.0f;
     public float padding = 1.0f;
+    public float projectileSpeed = 2f;
+    public float firingRate = 2f;
 
     float xmin;
     float xmax;
@@ -31,10 +34,26 @@ public class PlayerController : MonoBehaviour {
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-
-
+        
         float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
         transform.position = new Vector3(newX, transform.position.y, 0f);
 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) 
+            || Input.GetKeyDown(KeyCode.W))
+        {
+            InvokeRepeating("Fire", 0.000001f, firingRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow)
+            || Input.GetKeyUp(KeyCode.W))
+        {
+            CancelInvoke("Fire");
+        }
+    }
+
+    void Fire()
+    {
+        GameObject beam = Instantiate(beamPrefab, transform.position,
+                  Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
     }
 }
